127eca601c165120bdc24fbf8c11156c9f6b3dfe
murilo gomes dos santos <murilo29@hotmail.com.br>
2018-11-08 17:27:43 -0200

    initial commit

---
+++ js/produto.js
@@ -0,0 +1,10 @@
+var	inputTamanho	=	document.querySelector('[name=tamanho]')
+var	outputTamanho	=	document.querySelector('[name=valortamanho]')
+function	mostraTamanho(){
+					outputTamanho.value	=	inputTamanho.value
+	}
+inputTamanho.oninput	=	mostraTamanho
+
+inputTamanho.oninput	=	function() {
+    outputTamanho.value	=	inputTamanho.value
+}
